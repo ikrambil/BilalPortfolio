607b0e980c3f27503c593fe66a6cbfae00df9c88
Bilal Ikram <bilal0106ikram@gmail.com>
2024-05-11 02:29:48 -0400

    add safari conditional

--- components/background-animation.tsx
+++ components/background-animation.tsx
@@ -1,5 +1,3 @@
-// ./components/BackgroundAnimation.tsx
-
 "use client";
 import React, { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
@@ -21,6 +19,9 @@ const BackgroundAnimation: React.FC = () => {
 
   // Reference to the container to get its dimensions for boundary checks
   const containerRef = useRef<HTMLDivElement>(null);
+
+  // Detect Safari browser
+  const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
   // Update positions based on speed and current position
   const updatePosition = (
@@ -49,6 +50,7 @@ const BackgroundAnimation: React.FC = () => {
   };
 
   useEffect(() => {
+    if (isSafari) return; // Do not run the interval if on Safari
     const intervalId = setInterval(() => {
       updatePosition(x, y, speedX, speedY, setX, setY, setSpeedX, setSpeedY);
       updatePosition(x2, y2, speedX2, speedY2, setX2, setY2, setSpeedX2, setSpeedY2);
@@ -59,11 +61,11 @@ const BackgroundAnimation: React.FC = () => {
   return (
     <div ref={containerRef} className="h-[95vh] w-full overflow-hidden relative -z-10 !rounded-3xl m-[2.5vh] border-16 border-[#f6f2e5]">
       <motion.div
-        animate={{ x, y }}
+        animate={!isSafari ? { x, y } : {} } // Apply animations conditionally
         className="bg-[#bb843d] absolute h-[70.25rem] w-[70.25rem] blur-[10rem] rounded-full -z-[100]"
       />
       <motion.div
-        animate={{ x: x2, y: y2 }}
+        animate={!isSafari ? { x: x2, y: y2 } : {} } // Apply animations conditionally
         className="bg-[#9c86a7] absolute h-[70.25rem] w-[70.25rem] blur-[10rem] rounded-full -z-[100]"
       />
     </div>
